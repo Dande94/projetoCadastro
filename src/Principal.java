e81dfc6faa0498f91bfd030cbf0d9ba7069545f0
Anderson Nunes <98499523+Dande94@users.noreply.github.com>
2022-03-16 21:16:53 -0300

    trocandoNomeExemplo

--- src/Principal.java
+++ src/Principal.java
@@ -3,15 +3,15 @@ public class Principal {
 
 	public static void main(String[] args) {
 	
-		Pessoa pessoa = new Pessoa("Indianara", 31);
+		Pessoa pessoa = new Pessoa("Anderson", 28);
 		
 		
 		EntradaSaida.exibirPessoa(pessoa);
 		
 		Pessoa p = new Pessoa();
 		
-		p.setNome("Amâncio");
-		p.setIdade(33);
+		p.setNome("Anderson");
+		p.setIdade(28);
 
 		EntradaSaida.exibirPessoa(p);
 
